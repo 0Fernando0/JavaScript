b89826a4bb256e378daf2ea8d6fdde9a9962ce7e
Fernando Borges Pereira <fernandoborgespereira57@gmail.com>
2021-09-20 18:39:14 -0300

    add ex 06 ate 07
    fiz alguns exercicios e adicionei pequenos projetos (para treinamento)

---
+++ EXERCICIOS/EX008/index.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('você ultrapassou o limite permitido,MULTADO!')
+             }
+console.log('dirija sempre usando o cinto de segurança')
---
+++ EXERCICIOS/EX009/index.js
@@ -0,0 +1,7 @@
+var país = 'BRASIL'
+console.log(`vivendo em ${país}`)
+if (país == 'BRASIL') {
+    console.log('Brasileiro')
+} else{
+    console.log('Estrangeiro')
+}
